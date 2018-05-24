4aa15375456422e1d023c75162e6a123c1cd1ce1
Jaume Durany <jdurany@gmail.com>
2018-05-24 20:49:35 +0200

    Adding ExpensesSummary component to show a summary of the expenses in ExpensesDashboardPage

---
+++ src/components/ExpensesSummary.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import selectExpenses from '../selectors/expenses';
+import selectExpensesTotal from '../selectors/expenses-total';
+import numeral from 'numeral';
+
+export const ExpensesSummary = (props) => (
+  <div>
+    {
+      props.expenseCount === 0 ? (
+        <p>No expenses</p>
+      ) : (
+        <p>Viewing {props.expenseCount} expenses totalling {
+          numeral(props.expensesTotal / 100).format('$0,0.00')
+        }</p>
+      )
+    }
+  </div>
+);
+
+const mapStateToProps = (state) => {
+  const expenses = selectExpenses(state.expenses, state.filters);
+  return {
+    expenseCount: expenses.length,
+    expensesTotal: selectExpensesTotal(expenses)
+  };
+};
+
+export default connect(mapStateToProps)(ExpensesSummary);
